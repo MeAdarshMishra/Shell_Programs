be521f2c038c379cc0bcaf87c048c9e54c0a70bb
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2020-12-07 22:30:49 +0530

    Add files via upload
    This code help to compare whether two strings are same or not

---
+++ Samestring.sh
@@ -0,0 +1,10 @@
+echo "Enter First String: "
+read str1
+echo "Enter Second String: "
+read str2
+if [ $str1 = $str2 ]
+then
+echo "Both Strings are same!"
+else
+echo "Strings entered are different!"
+fi
